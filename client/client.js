9071299963b716d1a729bf07d8e9627b533402d5
Avijit Gupta <526avijit@gmail.com>
2015-07-06 16:21:57 +0530

    Directory structured according to docs

---
+++ client/client.js
@@ -0,0 +1,42 @@
+var thePlayers = Meteor.subscribe('thePlayers');
+Template.leaderboard.helpers({
+    'player': function() {
+        var currentUserId = Meteor.userId();
+        return PlayersList.find({createdBy: currentUserId},
+                                {sort: {score: -1, name: 1} });
+    },
+    'selectedClass': function() {
+        var playerId = this._id;
+        var selectedPlayer = Session.get('selectedPlayer');
+        if (selectedPlayer === playerId) {
+            return "selected";
+        }
+    },
+    'showSelectedPlayer': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        return PlayersList.findOne(selectedPlayer);
+    }
+})
+Template.leaderboard.events({
+    'click .player': function() {
+        var playerId = this._id;
+        Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        Meteor.call('modifyPlayerScore', selectedPlayer, 5);
+    },
+    'click .decrement': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        Meteor.call('modifyPlayerScore', selectedPlayer, -5);
+    },
+    'submit form': function(event) {
+        event.preventDefault();
+        var playerName = event.target.playerName.value;
+        Meteor.call('insertPlayerData', playerName);
+    },
+    'click .remove': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        Meteor.call('removePlayerData', selectedPlayer);
+    }
+})
